370e12a04e4a9a4762225dfe81d8e5176e242a33
Abhay <abhaysinghrana2002@gmail.com>
2026-02-28 12:26:06 +0530

    Added creating tags feature

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -156,3 +156,9 @@ class PrivateRecipeApiTest(TestCase):
         recipe = recipes[0]
         self.assertEqual(recipe.tags.count(), 2)
         self.assertIn(tag_indian, recipe.tags.all())
+        for tag in payload['tags']:
+            exists = recipe.tags.filter(
+                name=tag['name'],
+                user=self.user,
+            ).exists()
+            self.assertTrue(exists)
